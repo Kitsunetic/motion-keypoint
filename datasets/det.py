8d8bb98b7b0f15b63015200af2eed54625fba786
Kitsunetic <kitsune.guru@gmail.com>
2021-03-23 12:31:21 +0900

    effdet train

---
+++ datasets/det.py
@@ -0,0 +1,183 @@
+import json
+from copy import deepcopy
+from pathlib import Path
+
+import albumentations as A
+import cv2
+import imageio
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import utils
+from albumentations.pytorch import ToTensorV2
+from error_list import error_list
+from PIL import Image
+from sklearn.model_selection import KFold, StratifiedKFold
+from torch import nn, optim
+from torch.utils.data import DataLoader, Dataset, Subset
+
+from .common import HorizontalFlipEx, VerticalFlipEx
+
+
+class DetDataset(Dataset):
+    def __init__(self, config, files, keypoints, augmentation):
+        super().__init__()
+        self.config = config
+        self.files = files
+        self.keypoints = keypoints
+
+        T = []
+        # T.append(A.Crop(0, 28, 1920, 1080 - 28))  # 1920x1080 --> 1920x1024
+        T.append(A.Resize(config.input_height, config.input_width))
+        if augmentation:
+            T.append(A.ImageCompression())
+            T.append(A.ShiftScaleRotate(border_mode=cv2.BORDER_CONSTANT))
+            T.append(HorizontalFlipEx())
+            T.append(VerticalFlipEx())
+            T.append(A.RandomRotate90())
+            T.append(A.IAASharpen())  # 이거 뭔지?
+            T.append(A.Cutout())
+            T_ = []
+            T_.append(A.RandomBrightnessContrast())
+            T_.append(A.RandomGamma())
+            T_.append(A.RandomBrightness())
+            T_.append(A.RandomContrast())
+            T.append(A.OneOf(T_))
+            T.append(A.GaussNoise())
+            T.append(A.Blur())
+        T.append(A.Normalize())
+        T.append(ToTensorV2())
+
+        self.transform = A.Compose(
+            transforms=T,
+            bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"]),
+            # keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
+            # TODO 영역을 벗어난 keypoint는 그 영역의 한도 값으로 설정해줄 것?
+        )
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        file = str(self.files[idx])
+        image = imageio.imread(file)
+
+        keypoint = self.keypoints[idx]
+        box = utils.keypoint2box(keypoint, self.config.padding)
+        box = np.expand_dims(box, 0)
+        labels = np.array([0], dtype=np.int64)
+        a = self.transform(image=image, labels=labels, bboxes=box)
+
+        image = a["image"]
+        bbox = a["bboxes"][0]
+
+        return file, image, bbox
+
+
+class TestDetDataset(Dataset):
+    def __init__(self, config, files):
+        super().__init__()
+        self.config = config
+        self.files = files
+
+        T = []
+        # T.append(A.Crop(0, 28, 1920, 1080 - 28))  # 1920x1080 --> 1920x1024
+        T.append(A.Resize(config.input_height, config.input_width))
+
+        self.transform = A.Compose(transforms=T)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        file = str(self.files[idx])
+        image = imageio.imread(file)
+
+        a = self.transform(image=image)
+
+        image = a["image"]
+
+        return file, image
+
+
+def get_det_dataset(config, fold):
+    total_imgs = np.array(sorted(list((config.data_dir / "train_imgs").glob("*.jpg"))))
+    df = pd.read_csv("data/ori/train_df.csv")
+    total_keypoints = df.to_numpy()[:, 1:].astype(np.float32)
+    total_keypoints = np.stack([total_keypoints[:, 0::2], total_keypoints[:, 1::2]], axis=2)
+
+    # 오류가 있는 데이터는 학습에서 제외
+    total_imgs_, total_keypoints_ = [], []
+    for i in range(len(total_imgs)):
+        if i not in error_list:
+            total_imgs_.append(total_imgs[i])
+            total_keypoints_.append(total_keypoints[i])
+    total_imgs = np.array(total_imgs_)
+    total_keypoints = np.array(total_keypoints_)
+
+    # 파일 이름 앞 17자리를 group으로 이미지를 분류 (파일이 너무 잘 섞여도 안됨)
+    groups = []
+    last_group = 0
+    last_stem = total_imgs[0].name[:17]
+    for f in total_imgs:
+        stem = f.name[:17]
+        if stem == last_stem:
+            groups.append(last_group)
+        else:
+            last_group += 1
+            last_stem = stem
+            groups.append(last_group)
+
+    # KFold
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=config.seed)
+    indices = list(skf.split(total_imgs, groups))
+    train_idx, valid_idx = indices[fold - 1]
+
+    # 데이터셋 생성
+    ds_train = DetDataset(
+        config,
+        total_imgs[train_idx],
+        total_keypoints[train_idx],
+        augmentation=True,
+    )
+    ds_valid = DetDataset(
+        config,
+        total_imgs[valid_idx],
+        total_keypoints[valid_idx],
+        augmentation=False,
+    )
+    dl_train = DataLoader(
+        ds_train,
+        batch_size=config.batch_size,
+        num_workers=config.num_cpus,
+        shuffle=True,
+        pin_memory=True,
+    )
+    dl_valid = DataLoader(
+        ds_valid,
+        batch_size=config.batch_size,
+        num_workers=config.num_cpus,
+        shuffle=False,
+        pin_memory=True,
+    )
+
+    # 테스트 데이터셋
+    test_files = sorted(list((config.data_dir / "test_imgs").glob("*.jpg")))
+    with open("data/test_imgs_effdet/data.json", "r") as f:
+        data = json.load(f)
+        offsets = data["offset"]
+        ratios = data["ratio"]
+    ds_test = TestDetDataset(
+        config,
+        test_files,
+    )
+    dl_test = DataLoader(
+        ds_test,
+        batch_size=config.batch_size,
+        num_workers=config.num_cpus,
+        shuffle=False,
+        pin_memory=True,
+    )
+
+    return dl_train, dl_valid, dl_test
---
+++ main-effdet-train.py
@@ -0,0 +1,277 @@
+import argparse
+import json
+import math
+import sys
+from pathlib import Path
+
+import cv2
+import imageio
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+from sklearn.model_selection import KFold
+from torch import nn, optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import networks
+import options
+import utils
+from datasets import KeypointDataset, TestKeypointDataset, get_pose_datasets
+from error_list import error_list
+from losses import JointMSELoss, KeypointLoss, KeypointRMSE
+
+
+class DetTrainer:
+    def __init__(self, config, fold, checkpoint=None):
+        self.C = config
+        self.fold = fold
+
+        self.det_model = networks.EfficientDet(self.C.det_mdel)
+        self.det_model.cuda()
+
+        # Optimizer
+        if self.C.SAM:
+            self.optimizer = utils.SAM(self.det_model.parameters(), optim.AdamW, lr=self.C.lr)
+        else:
+            self.optimizer = optim.AdamW(self.det_model.parameters(), lr=self.C.lr)
+
+        # Scheduler
+        self.scheduler = ReduceLROnPlateau(self.optimizer, factor=0.5, patience=3, verbose=True)
+
+        self.epoch = 1
+        self.best_loss = math.inf
+        self.earlystop_cnt = 0
+
+        # Dataset
+        self.dl_train, self.dl_valid, self.dl_test = get_pose_datasets(self.C, self.fold)
+
+        # Load Checkpoint
+        if checkpoint is not None:
+            self.load(checkpoint)
+
+    def save(self, path):
+        torch.save(
+            {
+                "model": self.det_model.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+                "epoch": self.epoch,
+                "best_loss": self.best_loss,
+                "earlystop_cnt": self.earlystop_cnt,
+            },
+            path,
+        )
+
+    def load(self, path):
+        print("Load pretrained", path)
+        ckpt = torch.load(path)
+        self.det_model.load_state_dict(ckpt["model"])
+        self.optimizer.load_state_dict(ckpt["optimizer"])
+        self.epoch = ckpt["epoch"]
+        self.best_loss = ckpt["best_loss"]
+        self.earlystop_cnt = ckpt["earlystop_cnt"]
+
+    def close(self):
+        self.logger.close()
+
+    def train_loop(self):
+        self.det_model.train()
+
+        meanloss, meanrmse = utils.AverageMeter(), utils.AverageMeter()
+        with tqdm(
+            total=len(self.dl_train.dataset),
+            ncols=100,
+            leave=False,
+            file=sys.stdout,
+            desc=f"Train {self.epoch:03d}",
+        ) as t:
+            for files, imgs, keypoints, target_heatmaps, ratios in self.dl_train:
+                imgs_, target_heatmaps_ = imgs.cuda(), target_heatmaps.cuda()
+                pred_heatmaps_ = self.det_model(imgs_)
+                loss = self.criterion(pred_heatmaps_, target_heatmaps_)
+                rmse = self.criterion_rmse(pred_heatmaps_, target_heatmaps_, ratios.cuda())
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                if isinstance(self.optimizer, utils.SAM):
+                    self.optimizer.first_step()
+                    self.criterion(self.det_model(imgs_), target_heatmaps_).backward()
+                    self.optimizer.second_step()
+                else:
+                    self.optimizer.step()
+
+                meanloss.update(loss.item())
+                meanrmse.update(rmse.item())
+                t.set_postfix_str(f"loss: {loss.item():.6f}, rmse: {rmse.item():.6f}", refresh=False)
+                t.update(len(imgs))
+
+        return meanloss(), meanrmse()
+
+    @torch.no_grad()
+    def valid_loop(self):
+        self.det_model.eval()
+
+        meanloss, meanrmse = utils.AverageMeter(), utils.AverageMeter()
+        with tqdm(
+            total=len(self.dl_valid.dataset),
+            ncols=100,
+            leave=False,
+            file=sys.stdout,
+            desc=f"Valid {self.epoch:03d}",
+        ) as t:
+            for files, imgs, keypoints, target_heatmaps, ratios in self.dl_valid:
+                imgs_, target_heatmaps_ = imgs.cuda(), target_heatmaps.cuda()
+                pred_heatmaps_ = self.det_model(imgs_)
+                loss = self.criterion(pred_heatmaps_, target_heatmaps_)
+                rmse = self.criterion_rmse(pred_heatmaps_, target_heatmaps_, ratios.cuda())
+
+                meanloss.update(loss.item())
+                meanrmse.update(rmse.item())
+                t.set_postfix_str(f"loss: {loss.item():.6f}, rmse: {rmse.item():.6f}", refresh=False)
+                t.update(len(imgs))
+
+        return meanloss(), meanrmse()
+
+    def finetune_step1(self):
+        self.C.log.info("Finetune Step 1")
+        self.det_model.freeze_head()
+        for self.epoch in range(self.epoch, self.C.step1_epoch + 1):
+            tloss, trmse = self.train_loop()
+            vloss, vrmse = self.valid_loop()
+
+            self.C.log.info(
+                f"Epoch: {self.epoch:03d},",
+                f"loss: {tloss:.6f};{vloss:.6f},",
+                f"rmse {trmse:.6f};{vrmse:.6f}",
+            )
+            self.C.log.flush()
+
+            if self.best_loss > vloss:
+                self.best_loss = vloss
+                self.save(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+
+    def finetune_step2(self):
+        self.C.log.info("Finetune Step 2")
+        self.det_model.freeze_tail()
+        for self.epoch in range(self.epoch, self.C.step2_epoch + 1):
+            tloss, trmse = self.train_loop()
+            vloss, vrmse = self.valid_loop()
+
+            self.C.log.info(
+                f"Epoch: {self.epoch:03d},",
+                f"loss: {tloss:.6f};{vloss:.6f},",
+                f"rmse {trmse:.6f};{vrmse:.6f}",
+            )
+            self.C.log.flush()
+
+            if self.best_loss > vloss:
+                self.best_loss = vloss
+                self.save(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+
+    def finetune_step3(self):
+        self.C.log.info("Finetune Step 3")
+        self.det_model.unfreeze_all()
+        for self.epoch in range(self.epoch, self.C.step3_epoch + 1):
+            tloss, trmse = self.train_loop()
+            vloss, vrmse = self.valid_loop()
+
+            self.C.log.info(
+                f"Epoch: {self.epoch:03d},",
+                f"loss: {tloss:.6f};{vloss:.6f},",
+                f"rmse {trmse:.6f};{vrmse:.6f}",
+            )
+            self.C.log.flush()
+            self.scheduler.step(vloss)
+
+            if self.best_loss > vloss:
+                self.best_loss = vloss
+                self.earlystop_cnt = 0
+                self.save(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+            elif self.earlystop_cnt >= 10:
+                self.C.log.info(f"Stop training at epoch", self.epoch)
+                break
+            else:
+                self.earlystop_cnt += 1
+
+    def fit(self):
+        self.finetune_step1()
+        self.epoch += 1
+        # torch.cuda.empty_cache()
+        self.load(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+
+        self.finetune_step2()
+        self.epoch += 1
+        # torch.cuda.empty_cache()
+        self.load(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+
+        self.finetune_step3()
+        self.epoch += 1
+        # torch.cuda.empty_cache()
+        self.load(self.C.result_dir / f"ckpt-{self.C.uid}_{self.fold}.pth")
+
+    @torch.no_grad()
+    def evaluate(self, dl: DataLoader, file_out_dir=None):
+        self.det_model.eval()
+
+        meanloss, meanrmse = utils.AverageMeter(), utils.AverageMeter()
+        with tqdm(total=len(dl.dataset), ncols=100, file=sys.stdout) as t:
+            for files, imgs, keypoints, target_heatmaps, ratios in dl:
+                imgs_, target_heatmaps_ = imgs.cuda(), target_heatmaps.cuda()
+                pred_heatmaps_ = self.det_model(imgs_)
+                loss = self.criterion(pred_heatmaps_, target_heatmaps_)
+                rmse = self.criterion_rmse(pred_heatmaps_, target_heatmaps_, ratios.cuda())
+
+                if file_out_dir is not None:
+                    file_out_dir = Path(file_out_dir)
+                    file_out_dir.mkdir(parents=True, exist_ok=True)
+
+                    for file, img, pred_heatmap, target_heatmap, ratio in zip(
+                        files, imgs, pred_heatmaps_.cpu(), target_heatmaps, ratios
+                    ):
+                        file = Path(file)
+
+                        pred_keypoint = utils.heatmaps2keypoints(pred_heatmap).type(torch.float32)
+                        target_keypoint = utils.heatmaps2keypoints(target_heatmap).type(torch.float32)
+                        pred_keypoint = pred_keypoint * 4 / ratio.view(1, 2)
+                        target_keypoint = target_keypoint * 4 / ratio.view(1, 2)
+                        keypoint_rmse = (pred_keypoint - target_keypoint).square().mean().sqrt()
+
+                        img_np = utils.denormalize(img).permute(1, 2, 0).mul(255).type(torch.uint8).numpy()
+                        img_np = np.array(Image.fromarray(img_np))
+                        img_np = cv2.resize(img_np, (int(img_np.shape[1] / ratio[0]), int(img_np.shape[0] / ratio[1])))
+                        img_pred_keypoint = utils.draw_keypoints(img_np, pred_keypoint.type(torch.int64))
+                        img_target_keypoint = utils.draw_keypoints(img_np, target_keypoint.type(torch.int64))
+
+                        imageio.imwrite(file_out_dir / f"{file.stem}_{keypoint_rmse:.2f}.jpg", img_pred_keypoint)
+                        imageio.imwrite(file_out_dir / file.name, img_target_keypoint)
+
+                    meanloss.update(loss.item())
+                    meanrmse.update(rmse.item())
+                    t.set_postfix_str(f"loss: {loss.item():.6f}, rmse: {rmse.item():.6f}", refresh=False)
+                    t.update(len(imgs))
+
+        return meanloss(), meanrmse()
+
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("config_file", type=str)
+    args = args.parse_args(sys.argv[1:])
+
+    config = options.load_config(args.config_file)
+    for fold, checkpoint in zip(config.folds, config.checkpoints):
+        config.log.file.write("\r\n")
+        config.log.info("Fold", fold)
+        trainer = PoseTrainer(config, fold, checkpoint)
+        trainer.fit()
+
+        # validation exmaple 이미지 저장
+        trainer.evaluate(trainer.dl_valid, config.result_dir / "example" / f"valid_{fold}")
+
+
+if __name__ == "__main__":
+    main()
